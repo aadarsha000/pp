c862c3c6848e93cf43e4f5d393a69690a8b232e9
pratistha <spratistha295@gmail.com>
2026-03-27 09:59:38 +0545

    Add application and candidate management features with serializers, filters, and viewsets. Implemented ApplicationFilter for filtering applications, created serializers for application and candidate data handling, and established viewsets for CRUD operations. Updated URL routing to include candidates and applications endpoints.

---
+++ apps/candidates/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ApplicationViewSet, CandidateViewSet
+
+router = DefaultRouter()
+router.register(r'applications', ApplicationViewSet, basename='applications')
+# router.register(r'stages', StageViewSet, basename='stages')
+# router.register(r'documents', DocumentViewSet, basename='documents')
+router.register(r'candidates', CandidateViewSet, basename='candidates')
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+]
--- apps/candidates/views.py
+++ apps/candidates/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
+from candidates.models import Application, Candidate
+from candidates.serializers import ApplicationSerializer, ApplicationStageUpdateSerializer, CandidateSerializer, ApplicationCreateSerializer
+from candidates.filters import ApplicationFilter
+from users.permissions import IsRecruiterOrAdmin
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 # Create your views here.
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.select_related('candidate', 'job').prefetch_related('logs')
+    serializer_class = ApplicationSerializer
+    permission_classes = [IsRecruiterOrAdmin]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ApplicationFilter
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ApplicationCreateSerializer
+        if self.action == 'update_stage':
+            return ApplicationStageUpdateSerializer
+        return ApplicationSerializer
+
+    @action(detail=True, methods=['patch'])
+    def update_stage(self, request, pk=None):
+        application = self.get_object()
+        serializer = self.get_serializer(application, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class CandidateViewSet(viewsets.ModelViewSet):
+    queryset = Candidate.objects.prefetch_related('applications')
+    serializer_class = CandidateSerializer
+    permission_classes = [IsRecruiterOrAdmin]
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
